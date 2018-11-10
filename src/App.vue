6c6b8dd67d1c0d66450dc97f52175590cc98694f
jjungyeun <wjddusdl11@ajou.ac.kr>
2018-11-11 02:04:31 +0900

    dev-review

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
 <div id="app">
 
+    <h3>모임의 후기를 작성해주세요!</h3>
+
+    <b-carousel class="carousel" indicators controls :interval="0">
+        
+        <reviewSlide></reviewSlide>
+        <reviewSlide></reviewSlide>
+        <reviewSlide></reviewSlide>
+        <reviewSlide></reviewSlide>
+
+    </b-carousel>
+
+        <b-form-checkbox size="sm" style="display: block">다시 보지 않기</b-form-checkbox>
+        <b-btn class="btnGroup" size="sm">제출</b-btn> 
 
 </div>
 </template>
 
 <script>
+import reviewSlide from './components/reviewSlide.vue'
 export default {
   name: 'app',
   components: {
+      reviewSlide
   }
 }
 </script>
@@ -18,8 +33,24 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+  text-align: center;
   color: #2c3e50;
+  background: #bdbdbd;
+
+  max-width: 500px;
+  max-height: 480px;
+
+  padding: 15px;
 }
 
+.carousel {
+    height: 300px;
+}
+
+.btnGroup {
+    width: 100px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
 
 </style>
---
+++ src/components/reviewSlide.vue
@@ -0,0 +1,73 @@
+<template>
+    <b-carousel-slide class="slide">
+        <b-container>
+            <b-row>
+                <b-col>
+                    <h3>Title</h3>
+                    <h4>Date</h4>
+                    <br>
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col>
+                    <b-img class="profileImg" rounded="circle" src="https://picsum.photos/1024/480/?image=55" />
+                </b-col>
+                <b-col>
+                    <h4>NickName</h4>
+                    <star-rating id="starRating" :show-rating="false" :star-size="30" :increment="0.5"></star-rating>
+                    <b-form-select v-model="selected" :options="options" size="sm" />
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col>
+                    <br><br><br>
+                </b-col>
+            </b-row>
+        </b-container>
+
+    </b-carousel-slide>
+</template>
+
+<script>
+import StarRating from 'vue-star-rating'
+
+export default {
+    components:{
+        StarRating
+    },
+    data () {
+        return {
+            selected: null,
+            options: [
+                { value: null, text: '--멤버 리뷰--', disabled:true},
+                { value: 1, text: '매너 좋음'},
+                { value: 2, text: '매너 구림'},
+                { value: 3, text: '욕설'},
+                { value: 4, text: '그냥 좋음'},
+                { value: 5, text: '그냥 싫음'},
+                { value: 6, text: '잘 모르는 사람'},
+            ]
+        }
+    }
+}
+</script>
+
+
+<style>
+.slide {
+    height: 350px;
+}
+
+.profileImg {
+    width: 100px;
+    height: 100px;
+}
+
+div {
+    color: #2c3e50;
+}
+
+#starRating {
+    margin-bottom: 20px;
+}
+</style>
