2160057a2cdc6044de4c062f067c7725fde51eb6
SeoYunKyung <carys3115@naver.com>
2018-11-19 00:47:53 +0900

    * 달력 이용한 날짜 추가 * 슬라이더 바 이용한 인원 min과 max 측정 * 사진 등록시 미리보기 보여줌 * 그 외 필요 form내용 입력 가능
    필요 사항
    * map API 추가 후 location map 띄우기
    * 카테고리 조건 의논
    * validation

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import BootstrapVue from 'bootstrap-vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+import AirbnbStyleDatepicker from 'vue-airbnb-style-datepicker'
+import 'vue-airbnb-style-datepicker/dist/vue-airbnb-style-datepicker.min.css'
+
 Vue.config.productionTip = false
+
+const datepickerOptions = {}
+
+Vue.use(BootstrapVue)
+Vue.use(AirbnbStyleDatepicker, datepickerOptions)
 
 new Vue({
   render: h => h(App),
