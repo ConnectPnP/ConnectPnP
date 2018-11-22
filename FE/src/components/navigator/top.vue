51ea01bc8b955893eb063b8467dfbbb59df02bdb
gkgus <40718259+gkgus@users.noreply.github.com>
2018-11-23 01:00:43 +0900

    로그인/아웃 구현
    npm install vue-cookie --save
    (cookies 아님)
    로그인 상태에 따라 버튼, 프로필이 보이고 숨겨지도록 설정.

--- FE/src/components/navigator/top.vue
+++ FE/src/components/navigator/top.vue
@@ -8,8 +8,14 @@
         ConnectPnP
       </b-navbar-brand>
       <sidebar></sidebar>
-      <b-collapse is-nav id="nav_collapse">
+      <!--카카오 로그인 버튼-->
+      <button type="button" class="btn navbtn btn-primary"  v-if="loginStatus==false" v-on:click=loginWithKakao>로그인</button>
 
+      <button type="button" class="btn navbtn btn-primary" v-if="loginStatus" v-on:click=logoutWithKakao>로그아웃</button>
+
+      <button type="button" class="btn navbtn btn-primary" v-if="loginStatus==false" v-on:click="loginStatus">회원가입</button>
+
+      <b-collapse v-if="loginStatus" is-nav id="nav_collapse">
         <b-navbar-nav class="ml-auto">
           <b-nav-item-dropdown right>
             <template slot="button-content">
@@ -21,13 +27,17 @@
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
+
     </b-navbar>
+
   </div>
 </template>
 
 <script>
   import topItem from './topItem';
   import sidebar from './nav';
+  //import kakaoLogin from '../kakaoLogin';
+  /* eslint-disable no-console*/
   export default {
     name: 'top',
     components: { topItem , sidebar},
@@ -41,12 +51,88 @@
           default() { return "https://tistory1.daumcdn.net/tistory/user/1122646/profile/profileImg?v=1537856192204";}
         }
     },
+      mounted: function () {
+          const scriptId = 'kakao_login';
+          const isExist = !!document.getElementById(scriptId);
+          if (!isExist) {
+              const script = document.createElement('script');
+              script.src = 'https://developers.kakao.com/sdk/js/kakao.min.js';
+              script.onload = () => this.initiate();
+              script.onerror = error => this.handleError(error);
+              script.id = scriptId;
+              document.body.appendChild(script)
+          } else this.initiate()
+      },
+      computed:{
+          loginStatus(){
+              var cookie_status= this.$cookie.get('loginStatus');
+              console.log("loginStatus>>"+ cookie_status);
+              if(cookie_status=='login'){
+                  console.log(cookie_status=='login');
+                  return true
+              } else {
+                  console.log(cookie_status=='login');
+                  return false
+              }
+
+          }
+      },
+      methods: {
+          initiate: () => {
+              Kakao.init("08991fe24f4ff6700ae96d13f881ea66");
+          },
+          handleError: (err) => {
+              /* eslint-disable no-console*/
+              console.warn(`This component threw an error (in '${err.target.outerHTML}'):`, this)
+          },
+
+          loginWithKakao(){
+              Kakao.Auth.login({
+                  success: (authObj) => {
+                      this.$cookie.set('loginStatus','login', 1);
+                      console.log( this.$cookie.get('loginStatus'));
+                      location.href="/"
+                      Kakao.API.request({
+                          url: '/v1/user/me',
+                          success: function (res) {
+                              //console.log(res);
+                              var userID = res.id;      //유저의 카카오톡 고유 id
+                              var userNickName = res.properties.nickname; //유저가 등록한 별명
+                              var profile_image = res.properties.profile_image;   //유저의 프로필 이미지
+
+                              console.log(res);
+                              // console.log(userID);
+                              // console.log(profile_image);
+                              // console.log(userNickName);
+
+                          }
+                      })
+                  },
+                  fail: (err) => this.onFailure(err)
+              });
+          },
+          logoutWithKakao(){
+
+              Kakao.Auth.logout(function() {
+                  setTimeout(function(){
+                      location.href="/"
+                  },1000);//로그아웃 처리되는 타임을 임시적으로 1000설정
+              });
+              this.$cookie.set('loginStatus','logout', 1);
+              console.log( this.$cookie.get('loginStatus'));
+          }
+
+      }
   }
+
   
 </script>
 
 <style scoped>
   .top {
     padding-bottom : 30px;
+  }
+  .navbtn{
+    margin: 5px;
   }
 </style>
