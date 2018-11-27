9fbb8c0f447aab0033df739848af0040909d32f2
gkgus <40718259+gkgus@users.noreply.github.com>
2018-11-28 02:38:53 +0900

    회원가입 부분
    카카오 정보 가져와 submit 하도록

--- FE/src/components/signUp.vue
+++ FE/src/components/signUp.vue
@@ -9,48 +9,46 @@
             <p>Connect PnP와 함께 새로운 만남을 시작하세요!</p>
         </div>
 
+
         <div class="signUp-backGround" align="center">
             <div class="contact-form-title">
                 회원 가입
             </div>
 
-            <form>
-                <div class="inputGroup" >
-                    <div class="inputLabel">카카오 ID</div>
-                    <div class="col-sm-10 inputForm">
-                        <input type="text" class="form-control" id="kakaoID" placeholder="kakaoID" >
-                    </div>
-                </div>
+            <div class="contact-form-title">
+                반갑습니다 ___ 님
+            </div>
 
-                <div class="inputGroup">
-                    <div class="inputLabel">비밀번호</div>
-                    <div class="col-sm-10 inputForm">
-                        <input type="password" class="form-control" id="password" placeholder="Password">
-                    </div>
-                </div>
+            <div class="guid-msg">
+                아래 항목에 답하고 ConnectPnP 회원가입을 완료하세요!
+            </div>
 
 
+
+            <!-- <form action="http://localhost:3000/user" method="post">-->
                 <div class="inputLabel">관심 카테고리</div>
-                <div class="checkboxBackground">
-                <div class="checkboxGroup">
+                    <div class="checkboxBackground">
+                        <div class="checkboxGroup">
 
-                    <div  v-for="item in catagoryList" :key="item.id">
-                        <div class="custom-control custom-checkbox custom-control-inline singleCatagoryCheck">
-                            <input type="checkbox" class="custom-control-input" :id="item.id">
-                            <label class="custom-control-label" :for="item.id">{{item.catagoryName}}</label>
+                            <div  v-for="item in catagoryList" :key="item.id">
+                                <div class="custom-control custom-checkbox custom-control-inline singleCatagoryCheck">
+                                    <input type="checkbox" class="custom-control-input" :id="item.id" :name="item.name" :value="item.name" >
+                                    <label class="custom-control-label" :for="item.id">{{item.catagoryName}}</label>
+                                </div>
+                            </div>
+
+
                         </div>
                     </div>
 
 
-                </div>
 
-                </div>
-                <div >
-                    <div class="col-sm-10">
-                        <button type="submit" class="btn btn-primary">카카오톡으로 회원가입</button>
-                    </div>
-                </div>
-            </form>
+            <button class="btn btn-primary" @click="senddata">회원가입</button>
+
+
+
+
+
 
         </div>
 
@@ -60,10 +58,17 @@
 
 <script>
 
+
     export default {
+        /* eslint-disable no-console*/
         name: "signUp",
         data() {
             return {
+                signUpData:{
+                    kakaoID: String,
+                    userNickName: String,
+                    profileImg : String,
+                },
                 catagoryList: [
                     {   id: 1,
                         catagoryName: "catagory1"
@@ -84,6 +89,45 @@
                         catagoryName: "catagory6"
                     }
                 ]
+            }
+        },
+       mounted: function () {
+
+       },
+        methods:{
+            senddata(){
+                var userData = new FormData();
+                Kakao.API.request({
+                    url: '/v1/user/me',
+                    success: function (res) {
+                        //console.log(res);
+                        // var userID = res.id;      //유저의 카카오톡 고유 id
+                        //var userNickName = res.properties.nickname; //유저가 등록한 별명
+                        //var profile_image = res.properties.profile_image;   //유저의 프로필 이미지
+                        //console.log("image>>"+ profile_image);
+                        //console.log(res);
+                        this.kakaoID=res.id;
+                        this.userNickName=res.properties.nickname;
+                        this.profileImg=res.properties.profile_image;
+                        console.log( this.kakaoID);
+                        //userData.append('kakaoID',this.kakaoID);
+                        //userData.append('userNickName',this.userNickName);
+                        //userData.append('profileImg',this.profileImg);
+
+                        // console.log(userID);
+                        // console.log(profile_image);
+                        // console.log(userNickName);
+                    }
+                });
+                userData.set('tmp','hello!');
+                console.log(userData.get('tmp'));
+                this.$http.post("http://localhost:3000/user",userData)
+                    .then(response => {
+                        console.log(response);
+                    }, error => {
+                        console.log(error);
+                    });
+
             }
         }
 
@@ -115,9 +159,12 @@
     .contact-form-title{
         font-size: 27px;
         margin-top: 30px;
-        margin-bottom: 60px;
-    }
 
+    }
+    .guid-msg{
+        margin-top: 20px;
+        font-size: 20px;
+    }
     .inputGroup{
 
     }
