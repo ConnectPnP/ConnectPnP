b1a6eebcd316c9d8ea05056311a56d02f01f0dac
gkgus <40718259+gkgus@users.noreply.github.com>
2018-11-30 01:33:26 +0900

    Update main.js
    axios
    vue-axios
    vue-resource

--- FE/src/main.js
+++ FE/src/main.js
@@ -4,6 +4,9 @@ import App from './App.vue'
 import router from './router'
 import Vuesax from 'vuesax'
 import AirbnbStyleDatepicker from 'vue-airbnb-style-datepicker'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -12,13 +15,14 @@ import 'vue-awesome/icons';
 import 'vuesax/dist/vuesax.css'
 import 'vue-airbnb-style-datepicker/dist/vue-airbnb-style-datepicker.min.css'
 
+
+const VueResource = require('vue-resource')
 Vue.use(Vuesax);
 Vue.use(BootstrapVue);
-
 // vue-cookie
 var VueCookie = require('vue-cookie');
 Vue.use(VueCookie);
-
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false;
 
 const datepickerOptions = {};
