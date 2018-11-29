5c348fab8b51a3cad5b7594c50fb4ee6d424efe0
gkgus <40718259+gkgus@users.noreply.github.com>
2018-11-30 01:26:32 +0900

    상단 프로필 정보 띄우기
    로그인시 기존 유저인지 판별
                        - 기존 유저라면 새로고침
                        - 아니라면 회원가입 창으로
    유저 이름, 프로필 사진 띄우기

--- FE/src/components/navigator/top.vue
+++ FE/src/components/navigator/top.vue
@@ -9,21 +9,20 @@
       </b-navbar-brand>
       <sidebar></sidebar>
       <!--카카오 로그인 버튼-->
-      <button type="button" class="btn navbtn btn-primary"  v-if="loginStatus==false" v-on:click=loginWithKakao>로그인</button>
 
-      <button type="button" class="btn navbtn btn-primary" v-if="loginStatus" v-on:click=logoutWithKakao>로그아웃</button>
 
-      <button type="button" class="btn navbtn btn-primary" v-if="loginStatus==false" v-on:click="loginStatus">회원가입</button>
-
-      <b-collapse v-if="loginStatus" is-nav id="nav_collapse">
+      <b-collapse  is-nav id="nav_collapse">
         <b-navbar-nav class="ml-auto">
-          <b-nav-item-dropdown right>
+          <button type="button" class="btn navbtn btn-primary"  v-if="loginStatus==false" v-on:click=loginWithKakao>로그인</button>
+          <button type="button" class="btn navbtn btn-primary" v-if="loginStatus==false" v-on:click="loginWithKakao">회원가입</button>
+
+          <b-nav-item-dropdown right v-if="loginStatus">
             <template slot="button-content">
-              <b-img class="profileImg" rounded="circle" width="40px" stype="margin-right:5px" :src="profile_path" />
-              <em>  {{user_name}}</em>
+              <b-img class="profileImg" rounded="circle" width="40px" height="40px" stype="margin-right:5px" :src="userInfo[1]" />
+              <em>  {{userInfo[0]}}</em>
             </template>
             <b-dropdown-item href="/myPage">MyPage</b-dropdown-item>
-            <b-dropdown-item href="/">Signout</b-dropdown-item>
+            <b-dropdown-item  v-on:click=logoutWithKakao >Signout </b-dropdown-item>
           </b-nav-item-dropdown>
         </b-navbar-nav>
       </b-collapse>
@@ -34,23 +33,28 @@
 </template>
 
 <script>
+  /*
+  * <template slot="button-content">
+              <b-img class="profileImg" rounded="circle" width="40px" stype="margin-right:5px" :src="profile_path" />
+              <em>  {{user_name}}</em>
+            </template>
+  * */
+
+
   import topItem from './topItem';
   import sidebar from './nav';
+
   //import kakaoLogin from '../kakaoLogin';
   /* eslint-disable no-console*/
   export default {
     name: 'top',
     components: { topItem , sidebar},
-    props: {
-        user_name : {
-          type: String,
-          default() { return "hyeon";}
-        }, 
-        profile_path : {
-          type: String,
-          default() { return "https://tistory1.daumcdn.net/tistory/user/1122646/profile/profileImg?v=1537856192204";}
-        }
-    },
+    data(){
+          return{
+              user_name : String,
+              profile_path : String
+          }
+      },
       mounted: function () {
           const scriptId = 'kakao_login';
           const isExist = !!document.getElementById(scriptId);
@@ -62,8 +66,11 @@
               script.id = scriptId;
               document.body.appendChild(script)
           } else this.initiate()
+
+
       },
       computed:{
+        //현재 로그인 상태 확인, 쿠키로 저장.
           loginStatus(){
               var cookie_status= this.$cookie.get('loginStatus');
               console.log("loginStatus>>"+ cookie_status);
@@ -74,10 +81,22 @@
                   console.log(cookie_status=='login');
                   return false
               }
+          },
 
+          userInfo(){
+              //쿠키에 저장되어 있는 유저 정보 불러오기
+              var cookie_status= this.$cookie.get('loginStatus');
+              if(cookie_status=='login'){
+                  var userInfoName=this.$cookie.get('userName');
+                  var userInfoProfile = this.$cookie.get('profile_path');
+                  return [userInfoName,userInfoProfile]
+              } else{
+                  return["",""]
+              }
           }
       },
       methods: {
+          //카카오 api 사용을 위한 초기화
           initiate: () => {
               Kakao.init("08991fe24f4ff6700ae96d13f881ea66");
           },
@@ -86,33 +105,37 @@
               console.warn(`This component threw an error (in '${err.target.outerHTML}'):`, this)
           },
 
-          loginWithKakao(){
-              Kakao.Auth.login({
-                  success: (authObj) => {
-                      this.$cookie.set('loginStatus','login', 1);
-                      console.log( this.$cookie.get('loginStatus'));
-                      location.href="/"
-                      Kakao.API.request({
-                          url: '/v1/user/me',
-                          success: function (res) {
-                              //console.log(res);
-                              var userID = res.id;      //유저의 카카오톡 고유 id
-                              var userNickName = res.properties.nickname; //유저가 등록한 별명
-                              var profile_image = res.properties.profile_image;   //유저의 프로필 이미지
+          //로그인
+          async loginWithKakao(){
+              var loginResult = await Kakao.Auth.login();
+              console.log("loginResult>>"+loginResult);
 
-                              console.log(res);
-                              // console.log(userID);
-                              // console.log(profile_image);
-                              // console.log(userNickName);
+              //카카오 계정에서 유저 정보 얻기
+              var kakaoData = await Kakao.API.request({url: '/v1/user/me'});
+              var userID= kakaoData.id;
+              var getURL = "http://localhost:3000/user/"+userID;
+              var user_name = kakaoData.properties.nickname.toString();
+              var profile_path = kakaoData.properties.profile_image.toString();
+              this.$cookie.set('userName',user_name, 1);
+              this.$cookie.set('profile_path',profile_path, 1);
 
-                          }
-                      })
-                  },
-                  fail: (err) => this.onFailure(err)
-              });
+
+
+              var userResult = await this.$http.get(getURL);
+              if(userResult.data.user){ //유저일 경우 (이미 db에 유저 정보가 있다.) -> 새로고침
+                  this.$cookie.set('loginStatus','login', 1);
+                  console.log( this.$cookie.get('loginStatus'));
+                  location.href="/"
+
+              } else{   //유저가 아닌 경우 -> 회원가입 창으로
+                 location.href="/signUp"
+              }
+
+              //location.href="/"
           },
-          logoutWithKakao(){
 
+          //로그아웃
+          logoutWithKakao(){
               Kakao.Auth.logout(function() {
                   setTimeout(function(){
                       location.href="/"
@@ -120,7 +143,8 @@
               });
               this.$cookie.set('loginStatus','logout', 1);
               console.log( this.$cookie.get('loginStatus'));
-          }
+              location.href="/"
+          },
 
       }
   }
