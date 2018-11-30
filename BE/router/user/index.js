d063e1573ce1ff8be7ba538c1a22b805a1749279
gkgus <40718259+gkgus@users.noreply.github.com>
2018-11-30 23:11:03 +0900

    router 추가
    유저가 회원으로 등록되어 있는지 확인

---
+++ BE/router/user/index.js
@@ -0,0 +1,22 @@
+// /api/user/
+
+const express = require('express');
+const router = express.Router();
+const userCtrl = require('./user.controller');
+
+
+
+
+// create user
+router.post('/',userCtrl.create);
+//유저로 등록되어 있는지 확인
+router.get('/:id',userCtrl.findUser)
+// get user info
+router.get('/details/:id',userCtrl.getUser);
+
+// make & edit profile by user code
+router.post('/profile/:id', userCtrl.editProfile);
+router.post('/profile/files/:id', userCtrl.uploadAvatar); // profile photo
+
+
+module.exports = router;
