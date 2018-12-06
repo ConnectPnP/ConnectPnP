f654454b342d933946a27ddc09ac9e049a327ac4
hyeongyeong <33833731+hyeongyeong@users.noreply.github.com>
2018-12-06 21:41:08 +0900

    Create uploadCategory.js

---
+++ BE/middlewares/uploadCategory.js
@@ -0,0 +1,32 @@
+//https://wayhome25.github.io/nodejs/2017/02/21/nodejs-15-file-upload/
+
+const multer = require('multer');
+
+function uploadAvatar(req, res) {
+  const storage = multer.diskStorage({
+    destination: function (request, file, cb) {
+      cb(null, './files/category/');
+    },
+    filename: function (request, file, cb) {
+      const uploadedFile = {
+        name: file.originalname.split('.')[0],
+        ext: file.originalname.split('.').pop(),
+      };
+      cb(null, `${uploadedFile.name}.${uploadedFile.ext}`);
+    },
+  });
+  const upload = multer({ storage : storage }).fields([{ name: 'categoryFile' }]);
+  const promise = new Promise((resolve, reject) => {
+    upload(req, res, (err) => {
+      if (err) reject(err);
+      else {
+        resolve({
+          ok: 'ok',
+        });
+      }
+    });
+  });
+  return promise;
+}
+
+module.exports = uploadAvatar;
