2fb37a98f30c88b8082f2106aa7fb5764d999c63
hyeongyeong <kohy0329@naver.com>
2018-12-10 19:47:27 +0900

    Url 변경

--- BE/config/server.config.js
+++ BE/config/server.config.js
@@ -3,13 +3,13 @@ exports.dbUrl = function(){
 };
 
 exports.serverUrl = function() {
-  // return 'http://54.180.96.156:3000/'
-  return 'http://localhost:3000/'
+  return 'http://54.180.96.156:3000/'
+  // return 'http://localhost:3000/'
 };
 
 exports.serverFE = function() {
-  // return 'http://54.180.96.156:8080/'
-  return 'http://localhost:8080'
+  return 'http://54.180.96.156:8080/'
+  // return 'http://localhost:8080'
 };
 
 
--- FE/src/server.config.js
+++ FE/src/server.config.js
@@ -1,9 +1,9 @@
 exports.serverUrl = function() {
-    // return 'http://54.180.96.156:3000/'
-    return 'http://localhost:3000/'
+    return 'http://54.180.96.156:3000/'
+    // return 'http://localhost:3000/'
   };
 
   exports.serverFE = function() {
-    // return 'http://54.180.96.156:8080/'
-    return 'http://localhost:8080/'
+    return 'http://54.180.96.156:8080/'
+    // return 'http://localhost:8080/'
   };
